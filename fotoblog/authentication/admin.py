f3516d85d94c3e6b1f35ff0302ebc8d8a9e24bb1
Jm GERONIMI <spawn2x@protonmail.com>
2023-12-08 19:03:27 +0100

    dissociate projects

--- fotoblog/authentication/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from authentication.models import User
-
-
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active')
-
-
-admin.site.register(User, UserAdmin)
--- fotoblog/blog/static/js/header.js
+++
@@ -1,3 +0,0 @@
-// add padding-top to bady (if necessary)
-let navbar_height = document.querySelector('.navbar').offsetHeight;
-document.body.style.paddingTop = `${navbar_height}px`;
